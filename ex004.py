474ee5922865c283df4ae92b302b4c313dde47bd
rezende-marcus <mglrezende@yahoo.com.br>
2021-05-13 14:56:07 -0300

    ex004

---
+++ ex004.py
@@ -0,0 +1,10 @@
+a = input('Digite algo: ')
+print('O tipo primitivo desse valor é ', type(a))
+print('Só tem espaços?', a.isspace())
+print('É um numero?', a.isnumeric())
+print('É alfabetico? ', a.isalpha())
+print('É alfanumerico? ', a.isalnum())
+print('Está em maiúsculas? ', a.isupper())
+print('Está em minusculas? ', a.islower())
+print('Está capitalizada? ', a.istitle())
+
