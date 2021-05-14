b008badda93cdfb0c6fcc90a579fc657d3226669
rezende-marcus <mglrezende@yahoo.com.br>
2021-05-14 09:11:59 -0300

    Update ex005.py

---
+++ ex005.py
@@ -0,0 +1,6 @@
+n = int(input('Digite um numero: '))
+# a = n -1
+# s = n + 1
+# print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n, a, s))
+# primeira opção
+print('Analisando o valor {}, seu antecessor é {}, e o sucessor é {}'. format(n, (n-1), (n+1)))
