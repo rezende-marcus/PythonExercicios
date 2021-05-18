6af581388547f8031c6baad485c6bce8f242761d
rezende-marcus <mglrezende@yahoo.com.br>
2021-05-18 10:19:36 -0300

    Create ex016.py

---
+++ ex016.py
@@ -0,0 +1,8 @@
+'''from math import trunc
+#import math
+num = float(input('Digite um valor: '))
+#print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, math.trunc(num)))
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, trunc(num)))'''
+
+num = float(input('Digite um valor: '))
+print('O valor digitado foi {} e a porção inteira é {}'.format(num, int(num)))
