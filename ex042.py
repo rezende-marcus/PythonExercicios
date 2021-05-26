6dbf40bb3428aad1fb0850858e978b9d73baa702
rezende-marcus <mglrezende@yahoo.com.br>
2021-05-26 10:37:24 -0300

    ex042
    Informar equilatero, escaleno ou isoceles

---
+++ ex042.py
@@ -0,0 +1,15 @@
+r1 = float(input('Primeiro segmento: '))
+r2 = float(input('Segundo segmento: '))
+r3 = float(input('Terceiro segmeto: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os sementos acima PODEM FORMAR um triângulo!', end='')
+    #if r1 == r2 and r2 == r3:
+    if r1 == r2 == r3:
+        print('EQUILÁTERO!')
+    #if r1 != r2 != r3 != r1:
+    elif r1 != r2 != r3 != r1:
+        print('ESCALENO!')
+    else:
+        print('ISÓCELES!')
+else:
+    print('Os segmentos acima NÃO PODEM FORMAR triângulo!')
